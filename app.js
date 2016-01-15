355587fc8144201ab84b20e13e3b4657c956191f
Timothy Gregg <timtgregg@gmail.com>
2016-01-15 11:52:15 -0500

    added sum stylz

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var http = require('http');
 var request = require('request');
 var express = require('express');
-var tokens=require('./env.js');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient
 var format = require('util').format;
 var Client = require('coinbase').Client;
+
+var tokens=require('./env.js');
+var client = new Client({
+  'apiKey':tokens.apiKey,
+  'apiSecret':tokens.apiSecret
+});
 
 var app = express();
 
@@ -17,47 +22,29 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-//blockr api url
-//tack on btc address for address json info
-var blockrUrl = 'https://btc.blockr.io/api/v1/address/info/'
-
-var client = new Client({
-  'apiKey':tokens.apiKey,
-  'apiSecret':tokens.apiSecret
-});
-
 app.get('/', function (req, res) {
-
   var address=null;
-
   client.getAccount('primary', function(err, account) {
-
-    if (err) {
-      res.write(err);
-      res.end();
-    }
-
+    //gets primary coinbase wallet
+    if (err) throw err;
     account.getAddresses(null,function(err,addrs){
+      //gets most recent address
       address=addrs[0].address;
-
-      MongoClient.connect('mongodb://127.0.0.1:27017/once_off_btc_address',
-      function(err,db){
-
+      MongoClient.connect(
+        'mongodb://127.0.0.1:27017/once_off_btc_address',
+        function(err,db){
         if(err) throw err;
-
         var collection=db.collection('addresses');
-
         collection.find().toArray(function(err, results) {
           for (var entry in results) {
             console.dir(results[entry].address);
           }
         });
       })
-
     });
 
   });
-  res.render('index')
+  res.render('index',{})
 });
 
 app.post('/callback',function(req,res){
